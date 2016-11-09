509157c61b28c1f41984f92f8503361f54c278cf
Andy <andrew.sunglae.kim@gmail.com>
2016-11-08 21:53:39 -0500

    cleans react starter code

---
+++ src/config/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Route } from 'react-router'
+import App from '../containers/App';
+import Arbidue from '../containers/Arbidue'
+
+module.exports = (
+  <Route path='/'component={App}>
+    <Route path='/arbidue' component={Arbidue} />
+  </Route>
+)
---
+++ src/containers/Arbidue.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+
+class Arbidue extends Component {
+  constructor(){
+    super()
+    this.state = {}
+  }
+  render(){
+    return (
+      <div>
+        I.. am arbidue!
+      </div>
+    )
+  }
+}
+
+export default Arbidue
