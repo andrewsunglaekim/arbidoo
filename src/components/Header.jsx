f01a46a79be3683c58a39585ca9a921af65b704f
Adrian Maseda <amaseda@gmail.com>
2016-11-15 15:43:49 -0500

    updated header. working on level select.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,24 @@ import React, {Component} from 'react'
 import {Link} from 'react-router'
 
 class Header extends Component{
+  constructor(props){
+    super()
+    this.state = {
+      showLevels: "levels hidden"
+    }
+  }
+  openLevelMenu(e){
+    let showLevelState = this.state.showLevels == "levels" ? "levels hidden" : "levels"
+    this.setState({
+      showLevels: showLevelState
+    })
+  }
   render(){
     return (
       <header>
-        <h1><Link to={'/arbidue'}>ARBIDOO</Link></h1>
+        <span onClick={(e) => this.openLevelMenu(e)}>Difficulty</span>
+        <h1><Link to={'/arbidoo/100'}>ARBIDOO</Link></h1>
+        <span>Leaderboard</span>
       </header>
     )
   }
